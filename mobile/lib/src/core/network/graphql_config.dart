ab8cc008e919b6d4f69cc5047b0ad766f7eb8f67
Cotbert2 <mtaeojgdev@gmail.com>
2025-12-03 08:07:10 -0500

    upload apk

--- mobile/lib/src/core/network/graphql_config.dart
+++ mobile/lib/src/core/network/graphql_config.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
 class GraphQLConfig {
-  static const String _endpoint = 'http://localhost:3000/graphql';
+  static const String _endpoint = 'http://3.150.56.243:3000/graphql';
 
   static GraphQLClient getClient() {
     final HttpLink httpLink = HttpLink(_endpoint);
